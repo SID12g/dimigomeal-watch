3fbb3977f9f14cde4a1b5f82dfe435978ffdf821
SID12g <sid12g@naver.com>
2024-09-13 08:21:08 +0900

    Initial Commit

---
+++ DimigoMealWatch Watch App/DimigoMealWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DimigoMealWatchApp.swift
+//  DimigoMealWatch Watch App
+//
+//  Created by 조성민 on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DimigoMealWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
